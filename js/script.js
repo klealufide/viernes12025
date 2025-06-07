7d00fc0f134528dfa4fa27f41af1f0cd299a3d62
klealufide <kleal40321@ufide.ac.cr>
2025-06-06 20:08:08 -0600

    Javascript Parte I

---
+++ js/script.js
@@ -0,0 +1,123 @@
+let nombre = "Karol Leal";
+let edad = 35;
+let altura = 1.60;
+let esEstudiante = false;
+const mayorEdad = 18;
+
+console.log(nombre);
+console.log(edad);
+console.log(altura);
+console.log(esEstudiante);
+console.log(mayorEdad);
+
+let total = 0;
+
+console.log("Global " + total);
+
+function suma(a, b) {
+    let total = a + b;
+    console.log("Local " + total);
+}
+
+suma(4, 4);
+
+console.log("Global " + total);
+
+
+console.log(nombre.length);
+console.log(nombre.charAt(3));
+console.log(nombre.endsWith("Leal"));
+console.log(altura.toFixed(0));
+console.log(altura.toString());
+console.log(esEstudiante.toString());
+
+let mensaje = edad >= mayorEdad ? "Es mayor de edad " : "No es mayor de edad";
+
+console.log(mensaje);
+
+// if -  if / else - if / else if  / else
+edad = 10;
+
+if (edad >= mayorEdad) {
+    console.log("IF : Es mayor de edad");
+}
+
+if (edad >= mayorEdad) {
+    console.log("IF / ELSE : Es mayor de edad");
+} else {
+    console.log("IF / ELSE : No es mayor de edad");
+}
+
+
+let nota = 70;
+
+if (nota >= 90) {
+    console.log("Calificacion A");
+} else if (nota < 90 && nota >= 70) {
+    console.log("Calificacion B");
+} else {
+    console.log("Calificacion C");
+}
+
+
+// switch
+let semaforo = "verde";
+if (semaforo == "verde") {
+    console.log("SIga!");
+} else if (semaforo == "amarillo") {
+    console.log("Cuidado!");
+} else if (semaforo == "rojo") {
+    console.log("detengase!");
+} else {
+    console.log("Color no esta definido!");
+}
+semaforo = "rojo";
+
+switch (semaforo) {
+    case "verde":
+        console.log("SIga!");
+        break;
+    case "amarillo":
+        console.log("Cuidado!");
+        break;
+    case "rojo":
+        console.log("detengase!");
+        break;
+    default:
+        console.log("Color no esta definido !");
+        break;
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+// tabla dl 4
+for (let i = 0; i <= 10; i++) {
+    console.log("Tabla 4 * " + i + " = " + (i * 4));
+}
+
+let listaFrutas = ["melon", "banano", "sandia"];
+
+for (let i = 0; i < listaFrutas.length; i++) {
+    console.log(listaFrutas[i]);
+}
+
+console.log(listaFrutas[0]);
+
+for (x in listaFrutas) {
+    console.log(listaFrutas[x]);
+}
+edad = 25;
+
+//while solo ingresamos si cumple la condicion
+while (edad < 30) {
+    console.log("WHILE:" + edad);
+    edad++;
+}
+
+// DO while ingresamos al menos 1 vez
+
+do {
+    edad++;
+    console.log("DOWHILE:" + edad);
+} while (edad < 30)
