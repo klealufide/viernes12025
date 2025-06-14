65fc3ffe802c8bb2480533d63076b0cbfa6bfdf0
klealufide <kleal40321@ufide.ac.cr>
2025-06-13 19:59:52 -0600

    Semana 5

--- js/script.js
+++ js/script.js
@@ -1,123 +1,184 @@
-let nombre = "Karol Leal";
-let edad = 35;
-let altura = 1.60;
-let esEstudiante = false;
-const mayorEdad = 18;
-
-console.log(nombre);
-console.log(edad);
-console.log(altura);
-console.log(esEstudiante);
-console.log(mayorEdad);
-
-let total = 0;
-
-console.log("Global " + total);
-
-function suma(a, b) {
-    let total = a + b;
-    console.log("Local " + total);
-}
-
-suma(4, 4);
-
-console.log("Global " + total);
 
 
-console.log(nombre.length);
-console.log(nombre.charAt(3));
-console.log(nombre.endsWith("Leal"));
-console.log(altura.toFixed(0));
-console.log(altura.toString());
-console.log(esEstudiante.toString());
+document.addEventListener("DOMContentLoaded", function () {
+    let mensajeSpan = document.getElementById("mensaje");
+    mensajeSpan.style.display = "none";
+    let nombre = "Karol Leal";
+    let edad = 35;
+    let altura = 1.60;
+    let esEstudiante = false;
+    const mayorEdad = 18;
 
-let mensaje = edad >= mayorEdad ? "Es mayor de edad " : "No es mayor de edad";
+    console.log(nombre);
+    console.log(edad);
+    console.log(altura);
+    console.log(esEstudiante);
+    console.log(mayorEdad);
 
-console.log(mensaje);
+    let total = 0;
 
-// if -  if / else - if / else if  / else
-edad = 10;
+    console.log("Global " + total);
 
-if (edad >= mayorEdad) {
-    console.log("IF : Es mayor de edad");
-}
+    function suma(a, b) {
+        let total = a + b;
+        console.log("Local " + total);
+    }
 
-if (edad >= mayorEdad) {
-    console.log("IF / ELSE : Es mayor de edad");
-} else {
-    console.log("IF / ELSE : No es mayor de edad");
-}
+    suma(4, 4);
 
-
-let nota = 70;
-
-if (nota >= 90) {
-    console.log("Calificacion A");
-} else if (nota < 90 && nota >= 70) {
-    console.log("Calificacion B");
-} else {
-    console.log("Calificacion C");
-}
+    console.log("Global " + total);
 
 
-// switch
-let semaforo = "verde";
-if (semaforo == "verde") {
-    console.log("SIga!");
-} else if (semaforo == "amarillo") {
-    console.log("Cuidado!");
-} else if (semaforo == "rojo") {
-    console.log("detengase!");
-} else {
-    console.log("Color no esta definido!");
-}
-semaforo = "rojo";
+    console.log(nombre.length);
+    console.log(nombre.charAt(3));
+    console.log(nombre.endsWith("Leal"));
+    console.log(altura.toFixed(0));
+    console.log(altura.toString());
+    console.log(esEstudiante.toString());
 
-switch (semaforo) {
-    case "verde":
+    let mensaje = edad >= mayorEdad ? "Es mayor de edad " : "No es mayor de edad";
+
+    console.log(mensaje);
+
+    // if -  if / else - if / else if  / else
+    edad = 10;
+
+    if (edad >= mayorEdad) {
+        console.log("IF : Es mayor de edad");
+    }
+
+    if (edad >= mayorEdad) {
+        console.log("IF / ELSE : Es mayor de edad");
+    } else {
+        console.log("IF / ELSE : No es mayor de edad");
+    }
+
+
+    let nota = 70;
+
+    if (nota >= 90) {
+        console.log("Calificacion A");
+    } else if (nota < 90 && nota >= 70) {
+        console.log("Calificacion B");
+    } else {
+        console.log("Calificacion C");
+    }
+
+
+    // switch
+    let semaforo = "verde";
+    if (semaforo == "verde") {
         console.log("SIga!");
-        break;
-    case "amarillo":
+    } else if (semaforo == "amarillo") {
         console.log("Cuidado!");
-        break;
-    case "rojo":
+    } else if (semaforo == "rojo") {
         console.log("detengase!");
-        break;
-    default:
-        console.log("Color no esta definido !");
-        break;
-}
+    } else {
+        console.log("Color no esta definido!");
+    }
+    semaforo = "rojo";
 
-for (let i = 0; i < 10; i++) {
-    console.log(i);
-}
-// tabla dl 4
-for (let i = 0; i <= 10; i++) {
-    console.log("Tabla 4 * " + i + " = " + (i * 4));
-}
+    switch (semaforo) {
+        case "verde":
+            console.log("SIga!");
+            break;
+        case "amarillo":
+            console.log("Cuidado!");
+            break;
+        case "rojo":
+            console.log("detengase!");
+            break;
+        default:
+            console.log("Color no esta definido !");
+            break;
+    }
 
-let listaFrutas = ["melon", "banano", "sandia"];
+    for (let i = 0; i < 10; i++) {
+        console.log(i);
+    }
+    // tabla dl 4
+    for (let i = 0; i <= 10; i++) {
+        console.log("Tabla 4 * " + i + " = " + (i * 4));
+    }
 
-for (let i = 0; i < listaFrutas.length; i++) {
-    console.log(listaFrutas[i]);
-}
+    let listaFrutas = ["melon", "banano", "sandia"];
 
-console.log(listaFrutas[0]);
+    for (let i = 0; i < listaFrutas.length; i++) {
+        console.log(listaFrutas[i]);
+    }
 
-for (x in listaFrutas) {
-    console.log(listaFrutas[x]);
-}
-edad = 25;
+    console.log(listaFrutas[0]);
 
-//while solo ingresamos si cumple la condicion
-while (edad < 30) {
-    console.log("WHILE:" + edad);
-    edad++;
-}
+    for (x in listaFrutas) {
+        console.log(listaFrutas[x]);
+    }
+    edad = 25;
 
-// DO while ingresamos al menos 1 vez
+    //while solo ingresamos si cumple la condicion
+    while (edad < 30) {
+        console.log("WHILE:" + edad);
+        edad++;
+    }
 
-do {
-    edad++;
-    console.log("DOWHILE:" + edad);
-} while (edad < 30)
+    // DO while ingresamos al menos 1 vez
+
+    do {
+        edad++;
+        console.log("DOWHILE:" + edad);
+    } while (edad < 30)
+
+
+    function sumar(a, b) {
+        return a + b;
+    }
+
+    console.log(sumar(3, 5));
+
+    let resultado = sumar(9, 8);
+    console.log(resultado);
+
+
+    let titulo = document.getElementById("titulo");
+    titulo.innerText = "Semana 5";
+
+    let subtutilo = document.getElementById("subtutilo");
+    subtutilo.style.color = "red";
+    subtutilo.style.fontSize = "50px";
+
+    let listaTarea = document.getElementById("listaTareas");
+    let nuevaTarea = document.createElement("li");
+    nuevaTarea.innerText = "Tarea 4";
+    listaTarea.appendChild(nuevaTarea);
+
+    let link = document.getElementById("link");
+    link.setAttribute("href", "https://www.fidelitasvirtual.org/moodle3/login/index.php");
+
+    let agregarTareaBtn = document.getElementById("agregarTarea");
+
+    agregarTareaBtn.addEventListener("click", function () {
+        let nuevaTarea = document.getElementById("nuevaTarea");
+
+        if (nuevaTarea.value.length == 0) {
+            mensajeSpan.style.display = "block";
+            nuevaTarea.style.borderColor = "red";
+        } else {
+            let nuevaTareaElemento = document.createElement("li");
+            nuevaTareaElemento.innerText = nuevaTarea.value;
+            listaTarea.appendChild(nuevaTareaElemento);
+            nuevaTarea.value = "";
+            mensajeSpan.style.display = "none";
+            nuevaTarea.style.borderColor = "black";
+        }
+    });
+
+
+    let imagen = document.getElementById("imagen");
+
+    imagen.addEventListener("mouseenter", function () {
+        imagen.setAttribute("src", "./img/salon.jpg");
+    })
+
+    imagen.addEventListener("mouseleave", function () {
+        imagen.setAttribute("src", "./img/img1.jpg");
+    })
+})
