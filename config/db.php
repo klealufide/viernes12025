87b82a5d571238bd736b0b68a943af999b9089e6
klealufide <kleal40321@ufide.ac.cr>
2025-07-11 19:20:51 -0600

    CRUD

---
+++ config/db.php
@@ -0,0 +1,52 @@
+<?php 
+
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$database = "biblioteca";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if($conn->connect_error){
+    die($conn->connect_error);
+} else {
+    echo "La conexion OKAY";
+}
+
+/*
+$sql = "INSERT INTO  usuarios (usuario, clave, nombre, rol) VALUES ('trojas','123456','Tatiana','encargado')";
+
+if($conn->query($sql)){
+    echo "Registro creado exitosamente<br>";
+} else {
+    echo "error al agregar el registro<br>";
+}
+*/
+$sql = "SELECT * from usuarios";
+$result = $conn->query($sql);
+
+if($result->num_rows > 0){
+    while($row = $result->fetch_assoc()){
+        echo $row["id"]."-".$row["usuario"]."-".$row["clave"]."-".$row["nombre"]."-".$row["rol"]."<br>";
+    }
+} else {
+    echo "No hay registros<br>";
+}
+
+
+$sql = "UPDATE usuarios SET nombre = 'Karol Leal' where id=1";
+
+if($conn->query($sql)){
+    echo "Registro actualizado exitosamente<br>";
+} else {
+    echo "error al actualizar el registro<br>";
+}
+
+
+$sql = "DELETE FROM usuarios where id=3";
+
+if($conn->query($sql)){
+    echo "Registro elimnado exitosamente<br>";
+} else {
+    echo "error al eliminar el registro<br>";
+}
