2a18206e45bcb96f14fae944c030cc77c7aedbd4
klealufide <kleal40321@ufide.ac.cr>
2025-07-18 20:21:26 -0600

    Append, preppend, toggle, animate

--- js/script.js
+++ js/script.js
@@ -17,23 +17,52 @@ $(function () {
     let multi = $("#titulo, .grupo2, li");
     console.log(multi);
 
-    $("a").attr("href","https://youtube.com")
+    $("a").attr("href", "https://youtube.com")
 
-    $("#saludar").on("click", function(){
+    $("#saludar").on("click", function () {
         //text() html()
         $("#titulo").text("Semana 10 - Unidad 6");
-        $("#titulo").css("color","red");
+        $("#titulo").css("color", "red");
         $(".grupo1").addClass("grupo3");
         $(".grupo1").removeClass("grupo1");
     });
 
-    $("#btnAgregar").on("click", function(){
+    $("#btnAgregar").on("click", function () {
         let tarea = $("#tarea");
-       // tarea.val("nuevo texto");
-        if(tarea.val() == ""){
-            tarea.css("borderColor","red");
+        // tarea.val("nuevo texto");
+        if (tarea.val() == "") {
+            tarea.css("borderColor", "red");
         } else {
             tarea.css("borderColor", "black");
+            $("#listaTarea").prepend("<li>" + tarea.val() + "</li>"); // inicio
+            //  $("#listaTarea").append("<li>"+tarea.val()+"</li>"); // final
         }
     })
+
+    $("#mostrar").on("click", function () {
+        $("#cuadrado").fadeIn(1000);
+    })
+    $("#ocultar").on("click", function () {
+        $("#cuadrado").fadeOut(1000);
+    })
+
+    $("#agrandar").on("click", function () {
+        $("#cuadrado").animate({
+            width: "400px",
+            height: "400px",
+            opacity: 0.5
+        }, 1000)
+    })
+
+    $("#original").on("click", function () {
+        $("#cuadrado").animate({
+            width: "200px",
+            height: "200px"
+        }, 1000)
+    })
+
+    $("#cambioColor").on("click", function () {
+        $("#listaTarea").toggleClass("listaTarea");
+    })
+
 })
